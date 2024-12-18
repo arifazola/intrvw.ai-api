b01ae7e6c041586d6a8df40f73c9591f1832221c
Ari <afazola@gmail.com>
2024-12-18 16:27:44 +0700

    removed echo

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -149,7 +149,6 @@ class AuthController extends Controller
 
     public function requestOtpForResetPassword(Request $request){
         try{
-            echo"test";
             $otpGenerator = new OtpGenerator();
             $otp = $otpGenerator->generate();
             $saveOtp = Otp::create([
